a590630afdf57508373cd399f24bc5c983825feb
CarlosMoralesR <carlangetas007@hotmail.com>
2023-10-30 23:00:54 -0600

    feat(accounts): adding manage acounts page design

--- src/app/modules/admin/accounts/accounts.module.ts
+++ src/app/modules/admin/accounts/accounts.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageAccountsPageComponent } from './pages/manage-accounts-page/manage-accounts-page.component';
 import { AccountsRoutingModule } from './accounts-routing.module';
-
-
+// Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    ManageAccountsPageComponent
-  ],
+  declarations: [ManageAccountsPageComponent],
   imports: [
     CommonModule,
-    AccountsRoutingModule
-  ]
+    AccountsRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+  ],
 })
-export class AccountsModule { }
+export class AccountsModule {}
--- src/app/modules/admin/accounts/pages/manage-accounts-page/manage-accounts-page.component.ts
+++ src/app/modules/admin/accounts/pages/manage-accounts-page/manage-accounts-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage-accounts-page',
   templateUrl: './manage-accounts-page.component.html',
-  styleUrls: ['./manage-accounts-page.component.css']
+  styleUrls: ['./manage-accounts-page.component.css'],
 })
 export class ManageAccountsPageComponent {
-
+  randomColor: string = this.getRandomColor();
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }

003aa6025c7942db67ebb795b2d62b423fd3034c
PabloR9080 <a19216279@alumnos.uady.mx>
2023-11-10 15:05:05 -0600

    chore: wip add notes-component

--- src/app/modules/notes/components/add-note/add-note.component.ts
+++ src/app/modules/notes/components/add-note/add-note.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, of } from 'rxjs';
+import { Patient } from 'src/app/modules/patient/interfaces/patient.interfaces';
 import { PatientService } from 'src/app/modules/patient/services/patient.service';
 import { SelectPersonArgs } from 'src/app/shared/interfaces/credentials.interfaces';
 import { CredentialsService } from 'src/app/shared/services/credentials.service';
@@ -12,40 +13,36 @@ import { CredentialsService } from 'src/app/shared/services/credentials.service'
   styleUrls: ['./add-note.component.css']
 })
 export class AddNoteComponent implements OnInit{
-
-  public patient_id: string;
   public note_id: string;
   private dialogRef: any;
   public isSaving: boolean = false;
   public doctor_id: string = '';
   public toggle: boolean = true;
-  public selected_patient: string | undefined;
+  public selected_patient: Patient | undefined;
 
   constructor(private credentialsService: CredentialsService,
               private toastr: ToastrService,
               private patientsService: PatientService,
               @Inject(MAT_DIALOG_DATA) private data: any,
               private dialogService: MatDialog){
-              this.patient_id = '';
               this.note_id = '';
   }
 
   ngOnInit(): void {
-    this.patient_id = this.data.patient_id || '';
+    this.selected_patient = this.data.patient;
     this.note_id = this.data.note_id || '';
     this.dialogRef = this.data.dialogRef;
+  }
+
+  selectPatient(patient: Patient){
+    console.log(patient);
+    this.selected_patient = patient;
   }
 
   closeDialog(){
     this.dialogRef.close();
   }
-
-  selectPatient(id: string){
-    this.patient_id = id;
-    this.selected_patient = id;
-  }
-
-
+  
   requestPatients(args: SelectPersonArgs){
     this.doctor_id = this.credentialsService.user_credentials._id;
     return this.patientsService.getAllExistingPatients(this.doctor_id, args.search)
